1f629ab96d1132e828d835c1d2ae3a10ec1f701f
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-20 14:31:27 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,28 @@
+p=int(input("Enter the a number: "))
+n=int(str(p))
+print(n)
+"""
+le=0
+if n==0:
+    print("Zero")
+elif n==1:
+    print("One")
+elif n==2:
+    print("Two")
+elif n==3:
+    print("Three")
+elif n==4:
+    print("Four")
+elif n==5:
+    print("Five")
+elif n==6:
+    print("Six")
+elif n==7:
+    print("Seven")
+elif n==8:
+    print("Eight")
+elif n==9:
+    print("Nine")
+else:
+    print("Not a Digit")
+"""
