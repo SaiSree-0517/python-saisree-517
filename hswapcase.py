9c2dfbbffa543072250fe8f2d080cf629ddbb58e
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-21 14:44:09 +0530

    Create hswapcase.py

---
+++ hswapcase.py
@@ -0,0 +1,16 @@
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+    
+"""
+Input (stdin)
+HackerRank.com presents "Pythonist 2".
+Your Output (stdout)
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+Expected Output
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+"""
