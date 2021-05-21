d4c055e089bcb86f73149add2f15b7a4fe7ca9aa
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-21 15:14:11 +0530

    Add files via upload

---
+++ formattt.py
@@ -0,0 +1,12 @@
+name="CSE-A"
+print("Hello {} !How are you?".format(name))
+age=19
+print("Age is %d" %(age))
+print("Hello %s !Your Age is %d"%(name,age))
+per=8.9
+print("Hello %s !Your Age is %d and your grade is %.2f"%(name,age,per))
+a=65
+print("hexa %x"%(a))
+print("hexa %X"%(a))
+print("char %c"%(a))
+print("hexa %o"%(a))
