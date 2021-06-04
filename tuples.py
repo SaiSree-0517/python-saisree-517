695d2abc89bfeb1cbe70f387b849d45b5d79c03e
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-06-04 15:27:55 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,5 +1,5 @@
 #tuple creation
-b=("CSE","IT","Mech","ECE")
+b=("CSE","IT","Mech","ECE","CSE","AGRI","PT")
 print(b)
 print(type(b))
 print(len(b))#size of tuple
@@ -15,18 +15,63 @@ print("CSE is repeated %s times" %(b.count("CSE")))#counting the elements in the
 for i in b:#printing element by element
     print(i)
 print(*b)
+#slicing
+print(b[1])
+#fetch elements from index2 to 5-1
+print(b[2:5])
+#0to(5-1)
+print(b[:5])
+#start index 3 to end
+print(b[3:])
+#start to end+1
+print(b[:])
+#step count 2 and 3
+print(b[::2])
+print(b[::3])
+print(b[1:6:3])
+print(b[::-2])
+print(b[::-3])
+print(b[1:6:1])
+print(b[::1])
+print(b[::-1])
+#negative index fetching
+print(b[-5:-2])
+"""
+methods in tuples
+1.count
+2.index
+"""
+print(b.index("CSE"))
 #output
-('CSE', 'IT', 'Mech', 'ECE')
+('CSE', 'IT', 'Mech', 'ECE', 'CSE', 'AGRI', 'PT')
 <class 'tuple'>
-4
+7
 ('apple', 'mango', 'goa')
 <class 'tuple'>
 size of tuple is: 3
-max element is Mech
-min element is CSE
-CSE is repeated 1 times
+max element is PT
+min element is AGRI
+CSE is repeated 2 times
 CSE
 IT
 Mech
 ECE
-CSE IT Mech ECE
+CSE
+AGRI
+PT
+CSE IT Mech ECE CSE AGRI PT
+IT
+('Mech', 'ECE', 'CSE')
+('CSE', 'IT', 'Mech', 'ECE', 'CSE')
+('ECE', 'CSE', 'AGRI', 'PT')
+('CSE', 'IT', 'Mech', 'ECE', 'CSE', 'AGRI', 'PT')
+('CSE', 'Mech', 'CSE', 'PT')
+('CSE', 'ECE', 'PT')
+('IT', 'CSE')
+('PT', 'CSE', 'Mech', 'CSE')
+('PT', 'ECE', 'CSE')
+('IT', 'Mech', 'ECE', 'CSE', 'AGRI')
+('CSE', 'IT', 'Mech', 'ECE', 'CSE', 'AGRI', 'PT')
+('PT', 'AGRI', 'CSE', 'ECE', 'Mech', 'IT', 'CSE')
+('Mech', 'ECE', 'CSE')
+0
