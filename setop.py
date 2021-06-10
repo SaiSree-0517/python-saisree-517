a17ce3f03c5a68b3a97264978a0f6214a62a3655
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-06-10 13:16:39 +0530

    Update setop.py

--- setop.py
+++ setop.py
@@ -27,20 +27,30 @@ c=b.difference(a)
 print("difference",c)
 b.difference_update(a)
 print("difference_update",b)
-#issuperset()
+
+"""
+issuperset()-->Set X is said to be the superset of set Y if all elements of Y are in X.
+The issuperset() method returns True if a set has every elements of another set (passed as an argument).
+If not, it returns False.
+"""
+
 A = {1, 2, 3, 4, 5}
 B = {1, 2, 3}
 C = {1, 2, 3}
 print("A is superset of B or B is subset of A:",A.issuperset(B))#checks A is superset of B or not and returns True or False 
 print(B.issuperset(A))#checks B is superset of A or not and returns True or False  
 print(C.issuperset(B))#checks C is superset of B or not and returns True or False
-#issubset()
+"""
+issubset()-->The issubset() method returns True if all elements of a set A
+are present in another set B which is passed as an argument and returns false if all elements not present.
+a set X is a subset of a set Y if all elements of X are also elements of Y.
+"""
 A = {1, 2, 3}
 B = {1, 2, 3, 4, 5}
 C = {1, 2, 4, 5}
 print(A.issubset(B))#Returns True , A is subset of B
 print(B.issubset(A))# Returns False, B is not subset of A
-print(A.issubset(C))#Returns False, A is not subset of A
+print(A.issubset(C))#Returns False, A is not subset of C
 print(C.issubset(B))#Returns True , C is subset of B
 #output
 union {1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 17, 19}
