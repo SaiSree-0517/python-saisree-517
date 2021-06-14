d949bca74f30bf995799ad8ccdd096d4335d1c55
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-06-14 14:46:24 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -5,10 +5,10 @@ print(dict_ds)
 print(type(dict_ds))
 res=dict_ds[12]
 print("at key 12 the value is {}".format(res))
-dict_ds1={5:"CSE-A",5:"CSE-C","Name":"Priya","Fname":"Priya"}
+dict_ds1={5:"CSE-A",5:"CSE-C","Name":"Sai","Fname":"Sai"}
 print(dict_ds1)
 #modify the values
-dict_ds1["Fname"]="devi"
+dict_ds1["Fname"]="Sree"
 print(dict_ds1)
 #add new keys
 dict_ds1["Department"]="CSE"
@@ -17,7 +17,7 @@ print("new dict after adding new keyvalue",dict_ds1)
 {5: 'CSE', 12: 'IT', 3: 'ECE', 4: 'MECH'}
 <class 'dict'>
 at key 12 the value is IT
-{5: 'CSE-C', 'Name': 'Priya', 'Fname': 'Priya'}
-{5: 'CSE-C', 'Name': 'Priya', 'Fname': 'devi'}
-new dict after adding new keyvalue {5: 'CSE-C', 'Name': 'Priya', 'Fname': 'devi', 'Department': 'CSE'}
+{5: 'CSE-C', 'Name': 'Sai', 'Fname': 'Sai'}
+{5: 'CSE-C', 'Name': 'Sai', 'Fname': 'Sree'}
+new dict after adding new keyvalue {5: 'CSE-C', 'Name': 'Sai', 'Fname': 'Sree', 'Department': 'CSE'}
 """
